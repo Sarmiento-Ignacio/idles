fe7dff0891d8b993e1d912af8da7ac758b8f6f46
Sarmiento-Ignacio <150623783+Sarmiento-Ignacio@users.noreply.github.com>
2024-05-25 07:33:16 -0400

    asdasd

--- db/config.ts
+++ db/config.ts
@@ -1,4 +1,3 @@
-import Email from '@auth/core/providers/email';
 import { defineDb, defineTable, column } from 'astro:db';
 
 // https://astro.build/db/config
@@ -7,69 +6,29 @@ import { defineDb, defineTable, column } from 'astro:db';
 const User = defineTable({
   columns: {
 
-    id: column.number({ primaryKey: true }),
+    id: column.text({ primaryKey: true }),
     Email: column.text({ unique: true}),
-    usename: column.text({ unique: true}),
+    username: column.text({ unique: true}),
     password: column.text(),
-    registredAt: column.date(),
-    lastLogin: column.date(),
+    
   },
 
 });    
-const Roles = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true }),
-    name: column.text(),
-  },
-});
 
-const UserRoles = defineTable({
-  columns: {
-    userId: column.number({ references: () => User.columns.id }),
-    roleId: column.number({ references: ()  => Roles.columns.id }),
-  },
-});
-const PasswordResetTokens = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true }),
-    userId: column.number({ references: () => User.columns.id }),
-    token: column.text({ unique: true }),
-    expiresAt: column.date(),
-  },
-});
-const UserDetails = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true }),
-    userId: column.number({ references: () => User.columns.id }),
-    fullName: column.text(),
-    address: column.text(),
-    phoneNumber: column.text(),
-    
-  },
-});
-const UserActivity = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true }),
-    userId: column.number({ references: () => User.columns.id }),
-    action: column.text(),
-    timestamp: column.date(),
-    // Add more columns as needed
-  },
-});
 const Todo = defineTable({
   columns: {
-    id: column.number({ primaryKey: true }),
+    id: column.text({ primaryKey: true }),
     title: column.text(),
     description: column.text(),
-    userId: column.number({ references: () => User.columns.id }),
-    categoryId: column.number({ references: () => Category.columns.id }),
+    userId: column.text({ references: () => User.columns.id }),
+    categoryId: column.text({ references: () => Category.columns.id }),
   },
 
 });
 
 const Category = defineTable({
   columns: {
-    id: column.number({ primaryKey: true }),
+    id: column.text({ primaryKey: true }),
     label: column.text({ unique: true }),
   },
 });
@@ -79,11 +38,6 @@ const Category = defineTable({
 export default defineDb({
   tables: {
     User,
-    Roles,
-    UserRoles,
-    PasswordResetTokens,
-    UserDetails,
-    UserActivity,
     Todo,
     Category,
   },
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,57 @@
-import { db } from 'astro:db';
+import { Category, db, User, Todo } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(User).values([
+		{
+			id: "123345",
+			Email: "ignacio@gmail.com",
+			username: "ignacio",
+			password: "1234",
+		},
+		{
+			id: "1232345",
+			Email: "kimd@gmail.com",
+			username: "kimd",
+			password: "1234",
+		},
+
+	]);
+
+	await db.insert(Category).values([
+		{
+			id: "pofdggpodfop",
+			label: "programador"
+		},
+		{
+			id: "sdafsadf!@#",
+			label: "diseñador"
+		},
+	]);
+
+    await db.insert(Todo).values([
+		{
+			id: "pofdggpodfop",
+			title: "hacer una app",
+			description: "hacer una app de astro",
+			categoryId: "sdafsadf!@#",
+			userId: "123345"
+		},
+		{
+			id: "QWER##RA",
+			title: "hacer un diseño",
+			description: "hacer un diseño de astro",
+			categoryId: "sdafsadf!@#",
+			userId: "1232345"
+
+
+		}
+
+	]);
+
+
+
+
+
+
 }
